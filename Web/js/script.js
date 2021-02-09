6dd11a5617007eaf42f38fcd577030d3b975c4f4
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2021-02-09 14:58:42 -0300

    .

--- Web/js/script.js
+++ Web/js/script.js
@@ -27,20 +27,3 @@ if(window.SimpleForm) {
 	});
 }
 
-const menuItens = document.querySelectorAll('.introducao a[href^="#"]');
-
-menuItens.forEach(item => {
-	item.addEventListener('click', scrollToIdOnClick);
-})
-
-function scrollToIdOnClick(event){
-	event.preventDefault();
-	const element = event.target;
-	const id = element.getAttribute('href');
-	const to = document.querySelector(id).offsetTop;
-}
-
-window.scroll({
-	top: to,
-	behavior: "smooth",
-});
